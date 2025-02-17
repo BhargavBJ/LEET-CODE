613cac38288458b164b63888380f0b503f63577e
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-02-17 21:50:13 +0530

    Update Top-K-Frequent-Elements.py

--- Arrays & HashMap/MEDIUM/Top-K-Frequent-Elements.py
+++ Arrays & HashMap/MEDIUM/Top-K-Frequent-Elements.py
@@ -14,7 +14,6 @@ class Solution:
             res.append(arr.pop()[1])
         return res
     
-a = Solution()
-print(a.topKFrequent([1,1,1,2,2,2,3,3,4,4,4],2))
+
 
 # nums = [1,1,1,2,2,2,3] k = 2 RESULT: [1,2]
