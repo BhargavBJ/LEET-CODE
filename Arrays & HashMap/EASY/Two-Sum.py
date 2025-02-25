e580820729ca6750f137d9228d393313d02f9ed9
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-02-25 16:59:29 +0530

    Update Two-Sum.py

--- Arrays & HashMap/EASY/Two-Sum.py
+++ Arrays & HashMap/EASY/Two-Sum.py
@@ -1,11 +1,10 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        l=[]
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    l.append(i)
-                    l.append(j)
-        return l
+        l={}
+        for i, n in enumerate(nums):
+            d = target - n
+            if d in l:
+                return [l[d],i]
+            l[n]=i
 
 #Link : https://leetcode.com/problems/two-sum/description/
