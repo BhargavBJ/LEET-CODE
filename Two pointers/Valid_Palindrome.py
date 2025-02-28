9170924877c77bee52cb391c7179dbe85d8571f1
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-02-28 19:14:32 +0530

    Update Valid_Palindrome.py

--- Two pointers/Valid_Palindrome.py
+++ Two pointers/Valid_Palindrome.py
@@ -4,6 +4,7 @@ class Solution:
         li=[]
         for i in range(len(s)):
             if (s[i] >= 'a' and s[i]<='z') or (s[i]>='A' and s[i] <='Z') or (s[i].isnumeric()):
+            # Or if s[i].isalnum():
                 li.append(s[i].lower())
             else:
                 continue
