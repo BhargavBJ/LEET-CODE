53daddc830e43c8eacfb4745f66858f52e983b7a
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-02-28 19:08:32 +0530

    Update Valid_Palindrome.py

--- Two pointers/Valid_Palindrome.py
+++ Two pointers/Valid_Palindrome.py
@@ -1,5 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        ''' This brute force approach that has O(N) Time and space complexity Probably the easiest solution '''
         li=[]
         for i in range(len(s)):
             if (s[i] >= 'a' and s[i]<='z') or (s[i]>='A' and s[i] <='Z') or (s[i].isnumeric()):
@@ -7,3 +8,5 @@ class Solution:
             else:
                 continue
         return li == li[::-1]
+
+#Link : https://leetcode.com/problems/valid-palindrome/
