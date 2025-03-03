59431c47cd3178071db17c2433849b08e0e0430a
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-03-03 16:52:11 +0530

    Update 3Sum.py

--- Two pointers/3Sum.py
+++ Two pointers/3Sum.py
@@ -19,3 +19,5 @@ class Solution:
                     while(nums[l] == nums[l-1]) and l<r:
                         l+=1
         return res
+
+#Link : https://leetcode.com/problems/3sum/description/
