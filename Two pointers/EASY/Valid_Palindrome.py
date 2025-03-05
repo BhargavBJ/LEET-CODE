17e03add92603c236866c839c6908e71cb74f7f9
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-03-05 19:10:01 +0530

    Add files via upload

---
+++ Two pointers/EASY/Valid_Palindrome.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        li=[]
+        for i in range(len(s)):
+            if s[i].isalnum():# (s[i] >= 'a' and s[i]<='z') or (s[i]>='A' and s[i] <='Z') or (s[i].isnumeric()):
+                li.append(s[i].lower())
+            else:
+                continue
+        return li == li[::-1]
+    
+s= Solution()
+print(s.isPalindrome("A,bcBa"))
