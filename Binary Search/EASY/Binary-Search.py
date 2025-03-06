704c8738687d6f09f4805038f678496cf8b300be
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-03-06 09:57:30 +0530

    Add files via upload

---
+++ Binary Search/EASY/Binary-Search.py
@@ -0,0 +1,15 @@
+from typing import List
+class Solution:
+    def search(self, l: List[int], k: int) -> int:
+        i = 0
+        e = len(l) - 1
+        while i <= e:
+            mid = (i + e) // 2  
+            if l[mid] > k:
+                e = mid - 1
+            elif l[mid] < k:
+                i = mid + 1
+            else:
+                return mid
+        return -1
+        
