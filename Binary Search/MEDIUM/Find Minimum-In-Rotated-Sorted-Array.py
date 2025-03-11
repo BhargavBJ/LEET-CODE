bdb423a44cecccf420245ecc322c295efe0cf06e
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-03-11 19:34:23 +0530

    Update Find Minimum-In-Rotated-Sorted-Array.py

--- Binary Search/MEDIUM/Find Minimum-In-Rotated-Sorted-Array.py
+++ Binary Search/MEDIUM/Find Minimum-In-Rotated-Sorted-Array.py
@@ -1,6 +1,7 @@
 from typing import List
 class Solution:
     def findMin(self, nums: List[int]) -> int:
+        #There's a much easier approach listed under Basic Questions for beginners folder
         l, r = 0, len(nums) - 1
         while l < r:
             m = l + (r - l) // 2
@@ -9,3 +10,5 @@ class Solution:
             else:
                 l = m + 1
         return nums[l]
+
+#Link : https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
