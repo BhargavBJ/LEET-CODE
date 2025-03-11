ba64f2fdc639008cd03388e00d95a3c29bb3be69
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-03-11 19:30:27 +0530

    Update Find Minimum-In-Rotated-Sorted-Array.py

--- Basic Questions For Beginners/Find Minimum-In-Rotated-Sorted-Array.py
+++ Basic Questions For Beginners/Find Minimum-In-Rotated-Sorted-Array.py
@@ -1,6 +1,13 @@
 from typing import List
 class Solution:
     def findMin(self, nums: List[int]) -> int:
+        #Beginner Approach: 
+        return min(nums)
+        # OR
+        a = sorted(nums)
+        return a[0]
+        #If You don't think you are a beginner try the binary search approach
+        ''' Binary Search Approach
         l, r = 0, len(nums) - 1
         while l < r:
             m = l + (r - l) // 2
@@ -8,4 +15,4 @@ class Solution:
                 r = m
             else:
                 l = m + 1
-        return nums[l]
+        return nums[l]'''
