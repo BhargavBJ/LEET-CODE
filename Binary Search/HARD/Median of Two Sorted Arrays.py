e0ebaf9e9960683288ae30bf38fac8465b68b27c
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-03-12 22:05:25 +0530

    Add files via upload

---
+++ Binary Search/HARD/Median of Two Sorted Arrays.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        li =[]
+        for i in nums1:
+            li.append(i)
+        for i in nums2:
+            li.append(i)
+        l = sorted(li)
+        if len(l) % 2 == 0:
+            a = len(l)//2
+            b = a-1
+            s = (l[a]+l[b])/2
+            return s
+        elif len(l) % 2 ==1:
+            a = len(l) //2
+            return l[a]
