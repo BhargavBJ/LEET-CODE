ee07c669d182d673e32d305aab8a48cca7c41384
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-03-13 21:39:59 +0530

    Add files via upload

---
+++ Arrays & HashMap/HARD/Median of Two Sorted Arrays.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+    def median(self,a: List[int]) -> float:
+        if len(a) % 2 ==0:
+            e1 = len(a)//2
+            e2 = e1 -1
+            m = (a[e1]+a[e2])/2
+        else :
+            e = len(a)//2
+            m = a[e]
+        return m
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        c =sorted(nums1+nums2)
+        return float(self.median(c))
+    
+s = Solution()
+print(s.findMedianSortedArrays(nums1 = [1,3], nums2 = [2]))
