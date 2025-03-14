0d6ba10f71535a095d0d5a7978c78bb66014945d
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-03-14 16:34:18 +0530

    Add files via upload

---
+++ Linked Lists/MEDIUM/Reorder List.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head:
+            return
+        n =[]
+        cur =head
+        while cur:
+            n.append(cur)
+            cur = cur.next
+        i, j = 0, len(n)-1
+        while i<j:
+            n[i].next = n[j]
+            i+=1
+            if i>=j:
+                break
+            n[j].next=n[i]
+            j-=1
+        n[i].next = None
