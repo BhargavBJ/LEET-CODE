1bb4537fa6b03f4ef8c326342cb0271be39e936f
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-03-15 15:54:07 +0530

    Add files via upload

---
+++ Basic Questions For Beginners/Search a 2D Matrix.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        '''
+        for i in matrix:
+            for j in i:
+                if j==target:
+                    return True
+        return False'''
+        for i in matrix:
+            if target in i:
+                return True
+        return False
