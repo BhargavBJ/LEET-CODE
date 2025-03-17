8979ede66f872f3a50555051827c2828f71331ca
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-03-17 19:04:07 +0530

    Add files via upload

---
+++ Basic Questions For Beginners/Reverse Integer.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        a = str(abs(x))
+        b = a[::-1]
+        res = int(b) if x > 0 else -int(b)
+        if res < -2**31 or res > 2**31 - 1:
+            return 0
+        return res
