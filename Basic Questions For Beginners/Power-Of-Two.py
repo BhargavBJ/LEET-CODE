656f1fc6e2c752a409a0a9e48fed61bc50de94cf
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-03-25 21:33:06 +0530

    Update Power-Of-Two.py

--- Basic Questions For Beginners/Power-Of-Two.py
+++ Basic Questions For Beginners/Power-Of-Two.py
@@ -11,4 +11,4 @@ class Solution:
 
         #Bit manipulation Technique:
         return n>0 and (n&(n-1))==0
-#Link : https://leetcode.com/problems/power-of-two/description/
+#Link : https://leetcode.com/problems/power-of-two/
