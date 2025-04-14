88cb048778e035c80fcbc4e06a46f9d6074fba8a
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-04-14 16:35:44 +0530

    Create Single Number III.py

---
+++ Basic Questions For Beginners/Single Number III.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        a = {}
+        for n in nums:
+            a[n] = a.get(n, 0) + 1
+        li =[]
+        for num,count in a.items():
+            if count == 1:
+                li.append(num)
+        return li
