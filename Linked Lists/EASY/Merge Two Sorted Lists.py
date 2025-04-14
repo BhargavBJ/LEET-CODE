2a623134dd9a5571a20f8a7798879ef0ebfdbc0a
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-04-14 19:17:42 +0530

    Add files via upload

---
+++ Linked Lists/EASY/Merge Two Sorted Lists.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+from typing import Optional
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        li1, li2 = [], []
+        while list1:
+            li1.append(list1.val)
+            list1 = list1.next
+        while list2:
+            li2.append(list2.val)
+            list2 = list2.next
+        li1.extend(li2)
+        sorted_list = sorted(li1)
+        print(sorted_list)
+        link = ListNode()
+        dummy = link
+        for i in sorted_list:
+            dummy.next = ListNode(i)
+            dummy=dummy.next
+        return link.next
