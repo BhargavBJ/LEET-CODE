c3c21e828f6c96af9fcf27b7c9492f0b98d1ac94
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-04-30 12:24:41 +0530

    Create Finding-theduplicate-Number-Using-Functions.py

---
+++ Arrays & HashMap/EASY/Finding-theduplicate-Number-Using-Functions.py
@@ -0,0 +1,7 @@
+from typing import Counter
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        count = Counter(nums)
+        duplicates = [num for num, freq in count.items() if freq > 1]
+        print("Repeated elements:", duplicates[0])
+        return duplicates[0]
