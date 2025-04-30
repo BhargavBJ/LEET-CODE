3bbe8d95e3338957f79487a8c828ad6df12dba29
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-04-30 12:26:50 +0530

    Add files via upload

---
+++ Linked Lists/MEDIUM/Remove Nth Node From End of List.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        li =[]
+        while head:
+            li.append(head.val)
+            head = head.next
+        a = len(li)
+        remove = a - n
+        li.pop(remove)
+        print(li)
+        link = ListNode()
+        dummy = link
+        for i in li:
+            dummy.next = ListNode(i)
+            dummy=dummy.next
+        return link.next
