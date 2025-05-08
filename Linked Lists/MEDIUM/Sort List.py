fb66e410839f121d8596c578648d4ec6aeb9218d
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-05-08 22:10:04 +0530

    Create Sort List.py

---
+++ Linked Lists/MEDIUM/Sort List.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        li =[]
+        while head:
+            li.append(head.val)
+            head = head.next
+        li = sorted(li)
+        link = ListNode()
+        dummy = link
+        for i in li:
+            dummy.next = ListNode(i)
+            dummy=dummy.next
+        return link.next
