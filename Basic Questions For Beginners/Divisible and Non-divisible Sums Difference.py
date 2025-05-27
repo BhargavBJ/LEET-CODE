e73a64b6808a02237a22b5d183955e8032cc6b91
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-05-27 10:01:15 +0530

    Create Divisible and Non-divisible Sums Difference.py

---
+++ Basic Questions For Beginners/Divisible and Non-divisible Sums Difference.py
@@ -0,0 +1,11 @@
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        li1, li2 = [], []
+        for i in range(1,n+1,1):
+            if i % m == 0:
+                li2.append(i)
+            else:
+                li1.append(i)
+        return sum(li1) - sum(li2)
+      
+#Link: https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/
