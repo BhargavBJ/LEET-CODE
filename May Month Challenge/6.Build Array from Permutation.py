f71b12676dc1ff1ca3df1391eae0263c7720c983
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-06-09 12:26:09 +0530

    Create 6.Build Array from Permutation.py

---
+++ May Month Challenge/6.Build Array from Permutation.py
@@ -0,0 +1,5 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        return [nums[i] for i in nums]
+
+  # Link : https://leetcode.com/problems/build-array-from-permutation/description/?envType=daily-question&envId=2025-05-06
