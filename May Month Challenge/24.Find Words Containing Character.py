a2eaed0b9064066fa5fb4148b98e1f135bfd36a3
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-06-17 10:11:45 +0530

    Create 24.Find Words Containing Character.py

---
+++ May Month Challenge/24.Find Words Containing Character.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        li =[]
+        for i in range(len(words)):
+            if x in words[i]:
+                li.append(i)
+        print(li)
+        return li
+
+  # Link : https://leetcode.com/problems/find-words-containing-character/?envType=daily-question&envId=2025-05-24
