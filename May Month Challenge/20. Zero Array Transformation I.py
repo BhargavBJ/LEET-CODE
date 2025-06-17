6376a41bf7a62fdc4b5d7d3e1be8ae1d83a938c1
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-06-17 10:08:30 +0530

    Create 20. Zero Array Transformation I.py

---
+++ May Month Challenge/20. Zero Array Transformation I.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        n = len(nums)
+        diff = [0] * (n + 1)
+        for li, ri in queries:
+            diff[li] -= 1
+            if ri + 1 < n:
+                diff[ri + 1] += 1
+        sum_val = 0
+        for i in range(n):
+            sum_val += diff[i]
+            if nums[i] > -sum_val:
+                return False
+        return True
+
+# Link : https://leetcode.com/problems/zero-array-transformation-i/description/?envType=daily-question&envId=2025-05-20
